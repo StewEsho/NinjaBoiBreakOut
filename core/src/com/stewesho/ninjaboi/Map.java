ba293d63e4fd8d9980cdeb855b18d71f71b59fbf
Stew Esho <StewardEsho@gmail.com>
2016-12-10 12:51:19 -0500

    added Tile enums and all tiles

---
+++ core/src/com/stewesho/ninjaboi/Map.java
@@ -0,0 +1,75 @@
+package com.stewesho.ninjaboi;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+enum Tile{
+    ICE(0),
+    WOOD(1),
+    WALL(2),
+    TREASURE(3);
+    //fields
+    private final int id;
+    private final Texture spritesheet;
+    private final TextureRegion texture;
+
+    Tile (int id){
+        this.id = id;
+        this.spritesheet = new Texture("art/tiles.png");
+        this.texture = new TextureRegion(this.spritesheet, this.id*64, 0, 64, 64);
+    }
+
+    public int getId(){ return this.id; }
+    public TextureRegion getTexture(){ return this.texture; }
+}
+
+public class Map{
+    private TextureRegion region;
+    private Tile[][] map;
+
+    private final int MAPWIDTH  = 523;
+    private final int MAPHEIGHT = 523;
+
+    public Map(){
+        map = new Tile[MAPWIDTH][MAPWIDTH];
+        initMap();
+    }
+
+    protected void initMap(){
+            for (int i = 0; i < MAPWIDTH; i++){
+                for (int j = 0; j < MAPHEIGHT; j++){
+                    try{
+                        switch ((i+j) % 4){
+                        case 0:
+                            map[i][j] = Tile.ICE;
+                            break;
+                        case 1:
+                            map[i][j] = Tile.WOOD;
+                            break;
+                        case 2:
+                            map[i][j] = Tile.WALL;
+                            break;
+                        case 3:
+                            map[i][j] = Tile.TREASURE;
+                            break;
+                        }
+                    } catch (NullPointerException e){
+                        Gdx.app.log("ERROR", "Map not initialized");
+                    }
+                }
+            }
+        }
+
+    public TextureRegion getTexture(int x, int y) {
+        try{
+            return this.map[x][y].getTexture();
+        } catch (NullPointerException e){
+            Gdx.app.log("ERROR", "Map not initialized");
+            return null;
+        }
+    }
+
+}
--- core/src/com/stewesho/ninjaboi/NinjaBoiBreakOut.java
+++ core/src/com/stewesho/ninjaboi/NinjaBoiBreakOut.java
@@ -9,22 +9,29 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class NinjaBoiBreakOut extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+    Map map;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+        map = new Map(); //level (just the one room)
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor((float)0.1, (float)0.1, (float)0.1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
+        for (int i = 0; i < 15; i++){
+            for (int j = 0; j < 10; j++){
+                batch.draw(map.getTexture(i, j), i*64, j*64);
+            }
+        }
+
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
