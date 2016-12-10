8ce34249f458c94f8c1227e679416f5ba07cf06a
Stew Esho <StewardEsho@gmail.com>
2016-12-09 23:38:05 -0500

    inital commit

---
+++ desktop/src/com/stewesho/ninjaboi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stewesho.ninjaboi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stewesho.ninjaboi.NinjaBoiBreakOut;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NinjaBoiBreakOut(), config);
+	}
+}
